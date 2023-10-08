90c534b123745f362b3d04c2bdc24216171e7ce5
Zane <zane.zheng@gmail.com>
2023-10-08 17:50:30 +0800

    finetune with system/conversations.

---
+++ finetune.py
@@ -0,0 +1,162 @@
+import os
+import math
+import pathlib
+from typing import Optional, Dict
+from dataclasses import dataclass, field
+import json
+
+import torch
+from torch.utils.data import Dataset
+import transformers
+from transformers.training_args import TrainingArguments
+
+
+@dataclass
+class ModelArguments:
+    model_name_or_path: Optional[str] = field(default=None)
+
+
+@dataclass
+class DataArguments:
+    data_path: str = field(default=None)
+
+
+@dataclass
+class TrainingArguments(transformers.TrainingArguments):
+    cache_dir: Optional[str] = field(default=None)
+    optim: str = field(default="adamw_torch")
+    model_max_length: int = field(default=1024)
+    use_lora: bool = field(default=False)
+    lora_target: str = field(default=None)
+
+
+class SupervisedDataset(Dataset):
+    """Dataset for supervised fine-tuning."""
+
+    def __init__(
+        self,
+        data_path,
+        tokenizer,
+        model_max_length,
+    ):
+        super(SupervisedDataset, self).__init__()
+        self.data = json.load(open(data_path))
+        self.tokenizer = tokenizer
+        self.model_max_length = model_max_length
+        self.ignore_index = -100
+        self.system_title = tokenizer('<<<system>>>\n').input_ids
+        self.system_title_len = len(self.system_title)
+        self.conversations_title = tokenizer('\n\n<<<conversations>>>\n').input_ids
+        self.conversations_title_len = len(self.conversations_title)
+        self.ret_token = tokenizer('\n').input_ids
+        self.ret_token_len = len(self.ret_token)
+
+        item = self.preprocessing(self.data[0])
+        print("input:", self.tokenizer.decode(item["input_ids"]))
+        labels = []
+        for id_ in item["labels"]:
+            if id_ == -100:
+                continue
+
+            labels.append(id_)
+        print("label:", self.tokenizer.decode(labels))
+
+    def __len__(self):
+        return len(self.data)
+
+    def preprocessing(self, example):
+        input_ids = []
+        labels = []
+
+        if "system" in example:
+            system_ids = self.tokenizer.encode(example["system"])
+            input_ids += self.system_title + system_ids + self.conversations_title
+            labels += [self.ignore_index] * (len(self.system_title) + len(system_ids) + len(self.conversations_title))
+
+        for message in example["conversations"]:
+            from_ = message["from"]
+            value = message["value"]
+            value_ids = self.tokenizer.encode(value)
+
+            if from_ == "human":
+                input_ids += value_ids + self.ret_token
+                labels += [self.ignore_index] * (len(value_ids) + self.ret_token_len)
+            else:
+                input_ids += value_ids + self.ret_token
+                labels += value_ids + self.ret_token
+
+        input_ids.append(self.tokenizer.eos_token_id)
+        labels.append(self.tokenizer.eos_token_id)
+        input_ids = input_ids[: self.model_max_length]
+        labels = labels[: self.model_max_length]
+
+        input_ids += [self.tokenizer.pad_token_id] * (
+            self.model_max_length - len(input_ids)
+        )
+        labels += [self.ignore_index] * (self.model_max_length - len(labels))
+
+        input_ids = torch.LongTensor(input_ids)
+        labels = torch.LongTensor(labels)
+        attention_mask = input_ids.ne(self.tokenizer.pad_token_id)
+        return {
+            "input_ids": input_ids,
+            "labels": labels,
+            "attention_mask": attention_mask,
+        }
+
+    def __getitem__(self, idx) -> Dict[str, torch.Tensor]:
+        return self.preprocessing(self.data[idx])
+
+
+def train():
+    parser = transformers.HfArgumentParser(
+        (ModelArguments, DataArguments, TrainingArguments)
+    )
+    model_args, data_args, training_args = parser.parse_args_into_dataclasses()
+
+    model = transformers.AutoModelForCausalLM.from_pretrained(
+        model_args.model_name_or_path,
+        trust_remote_code=True,
+        cache_dir=training_args.cache_dir,
+    )
+    tokenizer = transformers.AutoTokenizer.from_pretrained(
+        model_args.model_name_or_path,
+        use_fast=False,
+        trust_remote_code=True,
+        model_max_length=training_args.model_max_length,
+        cache_dir=training_args.cache_dir,
+    )
+
+    tokenizer.pad_token_id = 0
+    tokenizer.bos_token_id = 1
+    tokenizer.eos_token_id = 2
+    #print(tokenizer.pad_token_id, tokenizer.bos_token_id, tokenizer.eos_token_id)
+
+    if training_args.use_lora:
+        from peft import LoraConfig, TaskType, get_peft_model
+
+        peft_config = LoraConfig(
+            task_type=TaskType.CAUSAL_LM,
+            target_modules=training_args.lora_target,
+            inference_mode=False,
+            r=8,
+            lora_alpha=32,
+            lora_dropout=0.1,
+        )
+        model.enable_input_require_grads()
+        model = get_peft_model(model, peft_config)
+        model.print_trainable_parameters()
+
+    dataset = SupervisedDataset(
+        data_args.data_path, tokenizer, training_args.model_max_length
+    )
+    trainer = transformers.Trainer(
+        model=model, args=training_args, train_dataset=dataset, tokenizer=tokenizer
+    )
+    trainer.train()
+    trainer.save_state()
+    trainer.save_model(output_dir=training_args.output_dir)
+
+
+if __name__ == "__main__":
+    train()
